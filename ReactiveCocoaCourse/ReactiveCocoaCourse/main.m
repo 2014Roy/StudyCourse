d828213551a445298496aaf1e247b63d39110996
roys <tl2012nana@163.com>
2015-10-29 16:17:39 +0800

    create project

---
+++ ReactiveCocoaCourse/ReactiveCocoaCourse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReactiveCocoaCourse
+//
+//  Created by Zonyet on 15/10/29.
+//  Copyright © 2015年 Zonyet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
